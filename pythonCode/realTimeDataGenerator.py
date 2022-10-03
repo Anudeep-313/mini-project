f59dcfcf549c4044feae88557d9f656fe35e6bbc
Jaideep Sharma <jaideep.sharma.cse20@itbhu.ac.in>
2022-10-03 12:59:22 +0530

    fixed database scripts

--- pythonCode/realTimeDataGenerator.py
+++ pythonCode/realTimeDataGenerator.py
@@ -20,9 +20,9 @@ else:
 sql = "INSERT INTO ORDERS (ROWID, ORDERID, ORDERDATE, SHIPDATE, SHIPMODE, CUSTOMERID, CUSTOMERNAME, SEGMENT, COUNTRY, CITY, STATE, POSTALCODE, REGION, POSTALID, CATEGORY, SUBCATEGORY, PRODUCTNAME, PRICE) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
 
 for i in range(df.shape[0]):
-    if(i%5 == 0):
-        sleep(60)
+    # if(i%5 == 0):
+        # sleep(60)
     count+=1
-    val = (count, df.at[i,'Order ID'], df.at[i,'Order Date'], df.at[i,'Ship Date'], df.at[i,'Ship Mode'], df.at[i,'Customer ID'], df.at[i,'Customer Name'], df.at[i,'Segment'], df.at[i,'Country'], df.at[i,'City'], df.at[i,'State'], df.at[i,'Postal Code'], df.at[i,'Region'], df.at[i,'Product ID'], df.at[i,'Category'], df.at[i,'Sub-Category'], df.at[i,'Product Name'], float(df.at[i,'Sales']))
+    val = (count, df.at[i,'Order ID'], df.at[i,'Order Date'], df.at[i,'Ship Date'], df.at[i,'Ship Mode'], df.at[i,'Customer ID'], df.at[i,'Customer Name'], df.at[i,'Segment'], df.at[i,'Country'], df.at[i,'City'], df.at[i,'State'], int(df.at[i,'Postal Code']), df.at[i,'Region'], df.at[i,'Product ID'], df.at[i,'Category'], df.at[i,'Sub-Category'], df.at[i,'Product Name'], float(df.at[i,'Sales']))
     mycursor.execute(sql,val)
     mydb.commit()
